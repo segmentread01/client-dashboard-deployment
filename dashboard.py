21f1431847f8eaf39ec579c368a99ce08dee9229
moi <moi>
2023-01-27 23:22:35 +0100

    add /predict dans url

--- dashboard.py
+++ dashboard.py
@@ -101,7 +101,7 @@ def get_shap_data():
 
 
 def main():
-    fast_uri = 'https://fast-api-app-deployment.herokuapp.com'
+    fast_uri = 'https://fast-api-app-deployment.herokuapp.com/predict'
     #fast_uri = "http://127.0.0.1:8000/predict"
 
     # sidebar
